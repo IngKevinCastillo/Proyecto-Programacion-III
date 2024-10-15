5217cbbe18edc69c943882dda11eeac837de850f
IngKevinCastillo <161144431+IngKevinCastillo@users.noreply.github.com>
2024-10-15 17:32:51 -0500

    Add files via upload

--- EconomiaPersonal/Presentacion/MenuOpciones.cs
+++ EconomiaPersonal/Presentacion/MenuOpciones.cs
@@ -1,12 +1,94 @@
-﻿using System;
+﻿using BBL;
+using Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Presentacion
 {
     public class MenuOpciones
     {
+        ServiviosApp ServiviosApp = new ServiviosApp();
+        public MenuOpciones() { }
+        public void RegistrarIngreso()
+        {
+            Console.Write("Digite el id del ingreso: "); int idIngreso = int.Parse(Console.ReadLine());
+            Console.Write("Digite la descripcion del ingreso: "); string descripcionIngreso = Console.ReadLine();
+            Console.Write("Digite la fecha del ingreso (DD-MM-YYYY): ");
+            string fecha = Console.ReadLine();
+            DateTime fechaIngreso;
+            if (!DateTime.TryParseExact(fecha, "dd-MM-yyyy", null, System.Globalization.DateTimeStyles.None, out fechaIngreso))
+            {
+                Console.WriteLine("Formato de fecha inválido. Debe ser DD-MM-YYYY.");
+                return;
+            }
+            Console.Write("Digite el monto del ingreso: "); double monto = double.Parse(Console.ReadLine());
+            Ingreso ingreso = new Ingreso(idIngreso, descripcionIngreso, fechaIngreso, monto);
+            ServiviosApp.RegistrarIngreso(ingreso);
+        }
+        public void EliminarIngreso()
+        {
+            Console.Write("Digite el id del ingreso a eliminar: "); int idIngreso = int.Parse(Console.ReadLine());
+            ServiviosApp.EliminarIngreso(idIngreso);
+        }
+        public void BuscarIngreso()
+        {
+            Console.Write("Digite el id del ingreso a eliminar: "); int idIngreso = int.Parse(Console.ReadLine());
+            ServiviosApp.BuscarIngreso(idIngreso);
+        }
+        public void RegistrarEgreso()
+        {
+            Console.Write("Digite el id del egreso: "); int idEgreso = int.Parse(Console.ReadLine());
+            Console.Write("Digite la descripcion del egreso: "); string descripcionGasto = Console.ReadLine();
+            Console.Write("Digite la fecha del egreso (DD-MM-YYYY): "); string fecha = Console.ReadLine();
+            DateTime fechaGasto;
+            if (!DateTime.TryParseExact(fecha, "dd-MM-yyyy", null, System.Globalization.DateTimeStyles.None, out fechaGasto))
+            {
+                Console.WriteLine("Formato de fecha inválido. Debe ser DD-MM-YYYY.");
+                return;
+            }
+            Console.Write("Digite la categoria del gasto: "); string categoria = Console.ReadLine();
+            Console.Write("Es un gasto prioritario?(SI/NO)"); string prioridadGasto = Console.ReadLine();
+            Console.Write("Digite el monto del egreso: "); double monto = double.Parse(Console.ReadLine());
+            Gasto gasto = new Gasto(idEgreso,descripcionGasto,categoria,prioridadGasto,fechaGasto,monto);
+            ServiviosApp.RegistrarGasto(gasto);
+        }
+        public void EliminarEgreso()
+        {
+            Console.Write("Digite el id del egreso a eliminar: "); int idEgreso = int.Parse(Console.ReadLine());
+            ServiviosApp.EliminarGasto(idEgreso);
+        }
+        public void BuscarEgreso()
+        {
+            Console.Write("Digite el id del egreso: "); int idEgreso = int.Parse(Console.ReadLine());
+            ServiviosApp.BuscarGasto(idEgreso);
+        }
+        public void consultaGasto()
+        {
+            
+        }
+        public void ConsultaGastosAnuales()
+        {
+            
+        }
+        public void ConsultaGastosMensuales()
+        {
+            
+        }
+        public void consultaIngreso()
+        {
+            
+        }
+        public void ConsultaIngresosAnuales()
+        {
+            
+        }
+        public void ConsultaIngresosMensuales()
+        {
+            
+        }
     }
 }
--- EconomiaPersonal/Presentacion/MenuPrincipal.cs
+++ EconomiaPersonal/Presentacion/MenuPrincipal.cs
@@ -8,32 +8,152 @@ namespace Presentacion
 {
     public class MenuPrincipal
     {
+        MenuOpciones menuOpciones = new MenuOpciones();
         public void Menu()
         {
             int OP = 0;
 
             do
             {
-                Console.WriteLine("*****CRUD de Personas*****");
-                Console.WriteLine("1. Agregar una persona.");
-                Console.WriteLine("2. Eliminar a una persona.");
-                Console.WriteLine("3. Buscar a una persona.");
-                Console.WriteLine("4. Lista de todas las personas.");
-                Console.WriteLine("5. Modificar datos de un Persona");
-                Console.WriteLine("6. Salir del programa.");
+                Console.WriteLine("\n*****ECONOMIA PERSONAL*****");
+                Console.WriteLine("1. Servicios de Ingresos.");
+                Console.WriteLine("2. Servicios de Egresos.");
+                Console.WriteLine("3. Servicios de Consulta.");
+                Console.WriteLine("4. Salir del programa.\n");
                 Console.Write("Seleccione una opcion: ");
                 OP = int.Parse(Console.ReadLine());
 
                 switch (OP)
                 {
                     case 1:
+                        ServiciosIngresos();
+                        break;
+                    case 2:
+                        ServiciosEgresos();
+                        break;
+                    case 3:
+                        ServiciosConsulta();
+                        break;
+                    case 4:
+                        Console.WriteLine("SALIENDO DE LA APP.............");
                         break;
                     default:
                         Console.WriteLine("Seleccione una opcion valida.");
                         break;
                 }
-            } while (OP != 6);
+            } while (OP != 4);
             Console.ReadKey();
+        }
+        private void ServiciosIngresos()
+        {
+            int OP = 0;
+            do
+            {
+                Console.WriteLine("\n*****SERVICIO INGRESOS*****");
+                Console.WriteLine("1. Registrar Ingreso.");
+                Console.WriteLine("2. Eliminar Ingreso.");
+                Console.WriteLine("3. Buscar Ingreso.");
+                Console.WriteLine("4. Regresar.\n");
+                Console.Write("Seleccione una opcion: ");
+                OP = int.Parse(Console.ReadLine());
+
+                switch (OP)
+                {
+                    case 1:
+                        menuOpciones.RegistrarIngreso();
+                        break;
+                    case 2:
+                        menuOpciones.EliminarIngreso();
+                        break;
+                    case 3:
+                        menuOpciones.BuscarIngreso();
+                        break;
+                    case 4:
+                        Console.WriteLine("REGRESANDO A MENU PPALL.......");
+                        break;
+                    default:
+                        Console.WriteLine("Seleccione una opcion valida.");
+                        break;
+                }
+            } while (OP != 4);
+        }
+        private void ServiciosEgresos()
+        {
+            int OP = 0;
+            do
+            {
+                Console.WriteLine("\n*****SERVICIO EGRESOS*****");
+                Console.WriteLine("1. Registrar Egreso.");
+                Console.WriteLine("2. Eliminar Egreso.");
+                Console.WriteLine("3. Buscar Egreso.");
+                Console.WriteLine("4. Regresar.\n");
+                Console.Write("Seleccione una opcion: ");
+                OP = int.Parse(Console.ReadLine());
+
+                switch (OP)
+                {
+                    case 1:
+                        menuOpciones.RegistrarEgreso();
+                        break;
+                    case 2:
+                        menuOpciones.EliminarEgreso();
+                        break;
+                    case 3:
+                        menuOpciones.BuscarEgreso();
+                        break;
+                    case 4:
+                        Console.WriteLine("REGRESANDO A MENU PPALL.......");
+                        break;
+                    default:
+                        Console.WriteLine("Seleccione una opcion valida.");
+                        break;
+                }
+            } while (OP != 4);
+        }
+        private void ServiciosConsulta()
+        {
+            int OP = 0;
+            do
+            {
+                Console.WriteLine("\n*****SERVICIO CONSULTAS*****");
+                Console.WriteLine("1. Consultar Gastos.");
+                Console.WriteLine("2. Consulta Gastos Anuales.");
+                Console.WriteLine("3. Consulta Gastos Mensuales.");
+                Console.WriteLine("4. Consultar Ingresos.");
+                Console.WriteLine("5. Consulta Ingresos Anuales.");
+                Console.WriteLine("6. Consulta Ingresos Mensuales.");
+                Console.WriteLine("7. Regresar.\n");
+                Console.Write("Seleccione una opcion: ");
+                OP = int.Parse(Console.ReadLine());
+
+                switch (OP)
+                {
+                    case 1:
+                        menuOpciones.consultaGasto();
+                        break;
+                    case 2:
+                        menuOpciones.ConsultaGastosMensuales();
+                        break;
+                    case 3:
+                        menuOpciones.ConsultaGastosAnuales();
+                        break;
+                    case 4:
+                        menuOpciones.consultaIngreso();
+                        break;
+                    case 5:
+                        menuOpciones.ConsultaIngresosMensuales();
+                        break;
+                    case 6:
+                        menuOpciones.ConsultaIngresosAnuales();
+                        break;
+                    case 7:
+                        Console.WriteLine("REGRESANDO A MENU PPALL.......");
+                        break;
+                    default:
+                        Console.WriteLine("Seleccione una opcion valida.");
+                        break;
+                }
+            } while (OP != 7);
         }
     }
 }
